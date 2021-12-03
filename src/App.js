cb94bcccf7cbac5822164232363e0782e5582c3d
AlvaroDDG86 <alvarodediosgarcia1986@gmail.com>
2021-12-03 15:07:51 +0100

    React calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 className="title">React CALC</h1>
+      <div className="calc">
+        <div className="operations">Operations: </div>
+        <div className="numbers">
+          <input className="field" type="number" placeholder="first"/>
+          <span></span>
+          <input className="field" type="number" placeholder="second"/>
+        </div>
+        <div className="actions">
+          <button className="btn">+</button>
+          <button className="btn">-</button>
+          <button className="btn">*</button>
+          <button className="btn">/</button>
+        </div>
+        <div className="footer">
+          <button className="btn btn--danger">reset</button>
+          <div className="result">Result:</div>
+        </div>
+      </div>
     </div>
   );
 }
